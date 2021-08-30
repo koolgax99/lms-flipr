34b9efc4b7b7b857572d185e90fa5a1a9efd9a5b
Nihar Sanda <nihar.sanda@gmail.com>
2021-08-30 18:41:20 +0530

    Added vuetify setup

---
+++ resources/js/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <component v-bind:is="layout"></component>
+  </v-app>
+</template>
+
+<script>
+import LoginSignupLayout from './layouts/LoginSignupLayout'
+import BasicAppLayout from './layouts/BasicAppLayout'
+export default {
+  computed: {
+    layout () {
+      return this.$store.getters.layout
+    }
+  },
+  components: {
+    'basic-app-layout': BasicAppLayout,
+    'login-signup-layout': LoginSignupLayout
+    // define as many layouts you want for the application
+  }
+}
+</script>
